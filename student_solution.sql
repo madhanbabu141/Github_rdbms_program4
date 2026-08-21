3bc046eb89f5db68f11517d7aeff6d2fdc520ebf
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 15:14:27 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,16 +1,1 @@
-CREATE TABLE Course (
-    CourseID INT PRIMARY KEY,
-    CourseName VARCHAR(30),
-    Credits INT,
-    DepartmentID INT
-);
 
-INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
-VALUES
-(101, 'Database Management', 4, 1),
-(102, 'Web Technology', 3, 1),
-(103, 'Data Mining', 4, 2);
-
-DESCRIBE Course;
-
-SELECT * FROM Course;
